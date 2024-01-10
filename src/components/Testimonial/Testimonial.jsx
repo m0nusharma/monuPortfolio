bec562ffdccb5560122faeb62ae01b5a91a2ddaf
m0nusharma <sharmamonu42969@gmail.com>
2024-01-10 21:21:29 +0530

    myPortfolio

---
+++ src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,24 @@
+
+import PropTypes from "prop-types";
+import SectionHeading from "../SectionHeading/SectionHeading";
+import Carousel from "../Carousel/Carousel";
+
+const Testimonial = ({ data }) => {
+  const { testimonialInfo, brandInfo } = data;
+  return (
+    <section className="section testimonials-section bg-g">
+      <div className="container">
+        <SectionHeading title="Languages i am Doing." subTitle="Languages" />
+       
+        <div className="testimonials-brand">
+          <Carousel data={brandInfo} />
+        </div>
+      </div>
+    </section >
+  )
+}
+Testimonial.propTypes = {
+  data: PropTypes.object
+}
+
+export default Testimonial
